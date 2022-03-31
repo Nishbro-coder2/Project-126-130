90d1e07534e7b91d3d4674b662944cd4445c015f
Nishbro-coder2 <98819146+Nishbro-coder2@users.noreply.github.com>
2022-03-31 19:31:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 song1 = "";
 song2 = "";
+song1_status = "";
+song2_status = "";
 
 function preload()
 {
@@ -47,23 +49,29 @@ function gotPoses(results)
 	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
 
 	scoreLeftWrist = results[0].pose.keypoints[9].score;
-
+	scoreRightWrist = results[0].pose.keypoints[10].score;
 		
   }
 }
 
 function draw() {
 	image(video, 0, 0, 600, 500);
-
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
 	fill("#FF0000");
 	stroke("#FF0000");
-	if(scoreLeftWrist >0.2){
-		circle(leftWristX,leftWristY,20);
-		number = Number(leftWristY);
-		removeD = floor(number);
-		volume = removeD/500;
-		document.getElementById("volume").innerHTML = "volume = "+ volume;
-		song.setVolume(volume);
+	if(scoreRightWrist > 0.2){
+		circle(rightWristX,rightWristY,20);
+		song2.stop();
+		if(song1_status == false){
+			song1.play();
+		}
 	}
-
+	if(scoreLeftWrist > 0.2){
+		circle(leftWristX,leftWristY,20);
+		song1.stop();
+		if(song2_status == false){
+			song2.play();
+		}
+	}
 }
